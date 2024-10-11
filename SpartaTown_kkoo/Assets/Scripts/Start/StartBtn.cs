2af90b4b1aa2e397ddc43d40491b0119885074d1
b-kkoo <bmcow331@naver.com>
2024-10-11 19:52:26 +0900

    [Fix] 스크립트 이름 수정
    StartScene.cs  -> StartBtn.cs

--- SpartaTown_kkoo/Assets/Scripts/Start/StartBtn.cs
+++ SpartaTown_kkoo/Assets/Scripts/Start/StartBtn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartScene : MonoBehaviour
+public class StartBtn : MonoBehaviour
 {
     public InputField playerNameInput;
     public string playerName;
